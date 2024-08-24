c702325d6108cbd8826a419d6d0ca610a47140a7
danny-adu <th0646894062@gmail.com>
2024-08-24 22:21:18 +0800

    add unit test

---
+++ AtomicCore.Integration.ClickHouseDbProviderUnitTest/ClickHouseDbProviderTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AtomicCore.Integration.ClickHouseDbProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicCore.Integration.ClickHouseDbProvider.Tests
+{
+    [TestClass()]
+    public class ClickHouseDbProviderTests
+    {
+        public ClickHouseDbProviderTests()
+        {
+            AtomicCore.AtomicKernel.Initialize();
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ AtomicCore.Integration.ClickHouseDbProviderUnitTest/DbHelperTests.cs
@@ -0,0 +1,14 @@
+﻿namespace AtomicCore.Integration.ClickHouseDbProviderUnitTest
+{
+    [TestClass()]
+    public class DbHelperTests
+    {
+        [TestMethod()]
+        public void GetDbTablesTest()
+        {
+            var tbs = DbHelper.GetDbTables(T4Config.global_ConnStr, T4Config.global_DbName);
+
+            Assert.IsTrue(null != tbs);
+        }
+    }
+}
