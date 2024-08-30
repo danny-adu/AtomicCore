8a6f93f1018dba2ba5bd550113e0505e6b109a43
danny-adu <th0646894062@gmail.com>
2024-08-30 20:45:24 +0800

    1

--- AtomicCore.Integration.ClickHouseDbProvider/TableEngine/IClickHouseTableEngine.cs
+++ AtomicCore.Integration.ClickHouseDbProvider/TableEngine/IClickHouseTableEngine.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Text;
 
 namespace AtomicCore.Integration.ClickHouseDbProvider
 {
@@ -11,14 +10,37 @@ namespace AtomicCore.Integration.ClickHouseDbProvider
     /// </summary>
     /// <typeparam name="M"></typeparam>
     public interface IClickHouseTableEngine<M>
-        where M : IClickHouseDbModel, new()
+        where M : IDbModel, IClickHouseDbModel, new()
     {
         /// <summary>
-        /// 指定更新操作
+        /// 更新操作（局部更新）
         /// </summary>
-        /// <param name="whereExp"></param>
-        /// <param name="updatePropertys"></param>
+        /// <param name="whereExp">需要被更新的条件</param>
+        /// <param name="updatePropertys">需要被替换或更新的属性</param>
         /// <returns></returns>
         DbNonRecord Update(Expression<Func<M, bool>> whereExp, Expression<Func<M, M>> updatePropertys);
+
+        /// <summary>
+        /// 更新操作（整体更新）
+        /// </summary>
+        /// <param name="whereExp">需要被更新的条件</param>
+        /// <param name="model">需要被整体替换的实体</param>
+        /// <returns></returns>
+        DbNonRecord Update(Expression<Func<M, bool>> whereExp, M model);
+
+        /// <summary>
+        /// 批量更新任务（在一个conn.open里执行多个更新,避免多次开关造成性能损失）
+        /// </summary>
+        /// <param name="taskList">任务数据</param>
+        /// <param name="enableSqlTransaction">是否启动SQL事务（对于单例调用最好启用，对于外层套用事务的不需要启动）</param>
+        /// <returns></returns>
+        DbNonRecord UpdateTask(IEnumerable<DbUpdateTaskData<M>> taskList, bool enableSqlTransaction = false);
+
+        /// <summary>
+        /// 删除操作
+        /// </summary>
+        /// <param name="deleteExp">删除条件</param>
+        /// <returns></returns>
+        DbNonRecord Delete(Expression<Func<M, bool>> deleteExp);
     }
 }
--- AtomicCore.Integration.ClickHouseDbProviderUnitTest/DataBase/Model/Member_UserBasics.cs
+++ AtomicCore.Integration.ClickHouseDbProviderUnitTest/DataBase/Model/Member_UserBasics.cs
@@ -1,5 +1,5 @@
-using System;
 using AtomicCore.DbProvider;
+using AtomicCore.Integration.ClickHouseDbProvider;
 
 namespace AtomicCore.Integration.ClickHouseDbProviderUnitTest
 {
@@ -8,38 +8,38 @@ namespace AtomicCore.Integration.ClickHouseDbProviderUnitTest
     /// </summary>
 	[DbDatabase(Name = "default")]
     [DbTable(Name = "Member_UserBasics")]
-	public class Member_UserBasics : IDbModel
-	{
-		        /// <summary>
+    public class Member_UserBasics : IDbModel, IClickHouseDbModel
+    {
+        /// <summary>
         /// 
         /// </summary>
-		[DbColumn(DbColumnName = "UserID", DbType = "Int32", IsDbPrimaryKey = true, IsDbGenerated =  false)]
+        [DbColumn(DbColumnName = "UserID", DbType = "Int32", IsDbPrimaryKey = true, IsDbGenerated = false)]
         public int UserID { get; set; } = 0;
 
         /// <summary>
         /// 
         /// </summary>
-		[DbColumn(DbColumnName = "UserName", DbType = "String", IsDbPrimaryKey = false, IsDbGenerated =  false)]
+		[DbColumn(DbColumnName = "UserName", DbType = "String", IsDbPrimaryKey = false, IsDbGenerated = false)]
         public string UserName { get; set; } = string.Empty;
 
         /// <summary>
         /// 
         /// </summary>
-		[DbColumn(DbColumnName = "UserAge", DbType = "Int32", IsDbPrimaryKey = false, IsDbGenerated =  false)]
+		[DbColumn(DbColumnName = "UserAge", DbType = "Int32", IsDbPrimaryKey = false, IsDbGenerated = false)]
         public int UserAge { get; set; } = 0;
 
         /// <summary>
         /// 
         /// </summary>
-		[DbColumn(DbColumnName = "UserIsBlock", DbType = "Bool", IsDbPrimaryKey = false, IsDbGenerated =  false)]
+		[DbColumn(DbColumnName = "UserIsBlock", DbType = "Bool", IsDbPrimaryKey = false, IsDbGenerated = false)]
         public bool UserIsBlock { get; set; } = false;
 
         /// <summary>
         /// 
         /// </summary>
-		[DbColumn(DbColumnName = "UserCreateAt", DbType = "DateTime", IsDbPrimaryKey = false, IsDbGenerated =  false)]
+		[DbColumn(DbColumnName = "UserCreateAt", DbType = "DateTime", IsDbPrimaryKey = false, IsDbGenerated = false)]
         public DateTime UserCreateAt { get; set; } = DateTime.Parse("1900-01-01");
 
 
-	}
+    }
 }
