f5890f20672cf6931e0ec27fd2862f2e538071ef
danny-adu <th0646894062@gmail.com>
2024-10-03 16:46:22 +0800

    1

---
+++ AtomicCore.Rocketmq.ClientUnitTest/Model/MyTransactionChecker.cs
@@ -0,0 +1,30 @@
+﻿using Org.Apache.Rocketmq;
+
+namespace AtomicCore.Rocketmq.ClientUnitTest
+{
+    public class MyTransactionChecker : ITransactionChecker
+    {
+        public TransactionResolution Check(MessageView message)
+        {
+            // 执行本地事务检查逻辑，例如从数据库检查事务是否已经提交
+            bool isCommitted = CheckTransactionStatus(message.MessageId);
+
+            // 根据检查结果返回事务的状态
+            if (isCommitted)
+            {
+                return TransactionResolution.Commit;
+            }
+            else
+            {
+                return TransactionResolution.Rollback;
+            }
+        }
+
+        private bool CheckTransactionStatus(string messageId)
+        {
+            // 模拟检查事务状态
+            // 这里可以连接数据库或者其他持久化存储来获取事务的实际状态
+            return true;  // 假设事务成功提交
+        }
+    }
+}
---
+++ AtomicCore.Rocketmq.ClientUnitTest/ProducerTests.cs
@@ -0,0 +1,74 @@
+﻿using AtomicCore.Rocketmq.ClientUnitTest;
+using System.Text;
+
+namespace Org.Apache.Rocketmq.Tests
+{
+    [TestClass()]
+    public class ProducerTests
+    {
+        private const string c_topices = "test";
+
+        [TestMethod()]
+        public void BeginTransactionTest()
+        {
+            /* 配置 NameServer 等参数 */
+            var config = new ClientConfig.Builder()
+                .SetEndpoints("127.0.0.1:9876")
+                .Build();
+
+            // 实例化my transaction checker
+            var transactionChecker = new MyTransactionChecker();
+            var producer = new Producer.Builder()
+                .SetClientConfig(config)
+                .SetTopics(c_topices)
+                .SetTransactionChecker(transactionChecker)
+                .Build()
+                .ConfigureAwait(false)
+                .GetAwaiter()
+                .GetResult();
+
+            // 开启事务
+            var transaction = producer.BeginTransaction();
+
+            // 创建消息
+            var message = new Message.Builder()
+                .SetTopic(c_topices)
+                .SetTag("TagA")
+                .SetBody(Encoding.UTF8.GetBytes("事务消息内容"))
+                .Build();
+
+            try
+            {
+                // 发送事务消息
+                var sendReceipt = producer.Send(message, transaction)
+                    .ConfigureAwait(false).GetAwaiter().GetResult();
+
+                // 执行本地事务逻辑
+                bool transactionSuccess = ExecuteLocalTransaction();
+
+                // 根据本地事务结果提交或回滚消息
+                if (transactionSuccess)
+                {
+                    transaction.Commit().ConfigureAwait(false).GetAwaiter().GetResult();
+                }
+                else
+                {
+                    transaction.Rollback().ConfigureAwait(false).GetAwaiter().GetResult();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("事务消息发送或处理失败: " + ex.Message);
+                transaction.Rollback().ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+
+            Assert.Fail();
+        }
+
+        private bool ExecuteLocalTransaction()
+        {
+            // 模拟本地事务处理，返回 true 表示成功，false 表示失败
+            return true;
+        }
+    }
+}
